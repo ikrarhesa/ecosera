95420a017fa50e832dbdc6721780ac61d16d3c6a
Ikrar Hesa Prasetya <ikrarhesa@hotmail.com>
2025-09-12 18:06:41 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Cart from "./pages/Cart";
 
 export default function App() {
   return (
-    <Shell>
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/etalase" element={<Etalase />} />
@@ -16,6 +16,6 @@ export default function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </Shell>
+    
   );
 }
