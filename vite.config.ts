cfd9b38555787500e65d359a40e71de5b184a865
Ikrar Hesa Prasetya <ikrarhesa@hotmail.com>
2025-09-13 01:31:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import json from '@rollup/plugin-json'
 
 export default defineConfig({
-  plugins: [react(), json()], // Adding JSON plugin for production build
-  build: {
-    rollupOptions: {
-      input: 'src/index.tsx', // Ensure this is correct
-    },
-  },
-  server: {
-    fs: {
-      allow: ['src', 'public'], // Make sure src is allowed in the dev environment
-    },
-  },
+  plugins: [react()],
 })
