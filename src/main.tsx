56eb0a8351f37b6510d0c1bc1d66265af8e5a6ef
Ikrar Hesa Prasetya <ikrarhesa@hotmail.com>
2026-03-03 12:24:41 +0700

    React Router Issue Fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import AppShell from './components/AppShell'
+import ScrollToTop from './components/ScrollToTop'
 import './index.css'
 import { CartProvider } from './context/CartContext'
 import { ToastProvider } from './context/ToastContext'
@@ -12,6 +13,7 @@ import { WishlistProvider } from './context/WishlistContext'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <ToastProvider>
         <ProductsProvider>
           <WishlistProvider>
